8207aac3ef09c38b915624b22598fa2a840421ca
Soptea Ioan <ioan.soptea@student.tuiasi.ro>
2026-03-10 16:42:59 +0000

    func3

--- Main.java
+++ Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String text = Files.readString(Path.of("input.txt"));
         String rezultat = text.replaceAll("\\p{Punct}", "").toUpperCase().replaceAll("\\d", "");
-        // System.out.println(rezultat); // lasa asta pentru pasul 3
+        System.out.println(rezultat);
     }
 }
