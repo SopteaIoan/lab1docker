139a5a75e5e24f06fbaa6a69d3e7f55f2e5b0cc6
Soptea Ioan <ioan.soptea@student.tuiasi.ro>
2026-03-10 16:50:18 +0000

    citire fisier

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         String text = Files.readString(Path.of("input.txt"));
-        String rezultat = text.replaceAll("\\p{Punct}", "").toUpperCase().replaceAll("\\d", "");
+        //String rezultat = text.replaceAll("\\p{Punct}", "").toUpperCase().replaceAll("\\d", "");
         System.out.println(rezultat);
     }
 }
