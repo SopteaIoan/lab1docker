9c54555fd811b1dac1c6ab66a4ab8ff65e43af52
Soptea Ioan <ioan.soptea@student.tuiasi.ro>
2026-03-10 16:41:25 +0000

    func 1 citire

--- Main.java
+++ Main.java
@@ -4,10 +4,6 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         String text = Files.readString(Path.of("input.txt"));
-        String rezultat = text.replaceAll("\\p{Punct}", "")
-                .toUpperCase()
-                .replaceAll("\\d", "");
-
-        System.out.println(rezultat);
+        System.out.println("Etapa 1: Citire efectuata.");
     }
 }
