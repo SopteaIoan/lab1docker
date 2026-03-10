f8d1d6ec4e650c5a17e2d2dcc895b84fd80e9a84
Soptea Ioan <ioan.soptea@student.tuiasi.ro>
2026-03-10 16:42:19 +0000

    func2

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         String text = Files.readString(Path.of("input.txt"));
-        System.out.println("Etapa 1: Citire efectuata.");
+        String rezultat = text.replaceAll("\\p{Punct}", "").toUpperCase().replaceAll("\\d", "");
+        // System.out.println(rezultat); // lasa asta pentru pasul 3
     }
 }
